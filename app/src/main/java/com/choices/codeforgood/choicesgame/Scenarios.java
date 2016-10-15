b2115a3a3b41e01b9b6eacc23a353009b2637bb7
Stephen Wan <wan183@osu.edu>
2016-10-15 01:11:23 -0400

    test commit

---
+++ app/src/main/java/com/choices/codeforgood/choicesgame/Scenarios.java
@@ -0,0 +1,36 @@
+package com.choices.codeforgood.choicesgame;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Scenarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scenarios);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView testtext = (TextView) findViewById(R.id.scenarios_maintext);
+        testtext.setText("this is what i want to put in for the scenario");
+
+    }
+
+}
